39ae25b063da03343e081e60d3e418057cb5b78b
graphffiti <56029683+graphffiti@users.noreply.github.com>
2021-01-21 03:15:43 +0800

    Initial TCP client with comments

---
+++ zmq-demo/stream-tcp/tcp_client.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost::asio;
+using ip::tcp;
+using std::string;
+using std::cout;
+using std::endl;
+
+/*
+int main()
+{
+    boost::asio::io_context io;
+
+    boost::asio::ip::tcp::socket client(io);
+
+    boost::asio::ip::tcp::endpoint server_details(boost::asio::ip::address::from_string("127.0.0.1"), 5555);
+
+    client.connect(server_details);
+
+    
+    
+    return 0;
+}
+*/
+
+int main() {
+
+    
+    //TEST
+     boost::asio::io_service io_service;
+//socket creation
+     tcp::socket socket(io_service);
+//connection
+     socket.connect( tcp::endpoint( boost::asio::ip::address::from_string("127.0.0.1"), 5555 ));
+
+     
+     // request/message from client
+     const string msg = "Hello from Client LORWIN!\n";
+     
+     boost::system::error_code error;
+     int y = 0;
+while(true){
+         std::string x;
+         std::cin >> x;
+     boost::asio::write( socket, boost::asio::buffer(msg), error );
+     if( !error ) {
+        cout << "Client sent hello message!" << endl;
+     }
+     else {
+        cout << "send failed: " << error.message() << endl;
+     }
+     
+     
+    
+     
+ // getting response from server
+    boost::asio::streambuf receive_buffer;
+    boost::asio::read(socket, receive_buffer, boost::asio::transfer_all(), error);
+    if( error && error != boost::asio::error::eof ) {
+        cout << "receive failed: " << error.message() << endl;
+    }
+    else {
+        const char* data = boost::asio::buffer_cast<const char*>(receive_buffer.data());
+        cout << data << endl;
+    }
+    cout << "end: " <<  endl;
+    
+}
+    return 0;
+}
